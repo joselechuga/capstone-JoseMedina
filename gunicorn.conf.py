87d73487413813dc180e3619845337b9a64a45de
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-10-31 17:37:07 -0300

    gunicorn update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import os
 
-bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+bind = '0.0.0.0:' + os.getenv("PORT", "8000")
 workers = 2  # Ajusta según los recursos disponibles
 timeout = 300
 graceful_timeout = 120
