54d772144db927d09a94b14df67dc1adcdfa348f
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-10-30 21:49:48 -0300

    intento fixed servidor RENDER

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'Hello, world!']
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,5 +2,6 @@ import os
 
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 workers = 2  # Ajusta según los recursos disponibles
-timeout = 120
+timeout = 300
+graceful_timeout = 120
 module = 'core.wsgi:application'
