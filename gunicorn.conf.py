0c9fab3c018983f1cb84279d7329ce19217635d9
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-11-04 16:38:57 -0300

    gunicorn update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,3 @@
-import os
-
-bind = '0.0.0.0:' + os.getenv("PORT", "4000")
-workers = 2  # Ajusta según los recursos disponibles
-timeout = 300
-graceful_timeout = 120
+bind = '0.0.0.0:8000'
+workers = 2
 module = 'core.wsgi:application'
