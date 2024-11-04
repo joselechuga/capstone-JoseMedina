ed575bac0c049c49c7767435379a488cd4d17a93
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-11-04 19:06:49 -0300

    try fix render

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 import os
 
 port = os.environ.get("PORT", "10000")  # Render especifica el puerto en una variable de entorno
-command = f"gunicorn core.wsgi:application --bind 0.0.0.0:{port}"
+command = f"gunicorn core.wsgi:application --bind 0.0.0.0:{port} --env WEB_CONCURRENCY=2"
 os.system(command)
