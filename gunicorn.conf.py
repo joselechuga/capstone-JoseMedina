dff2b4e2ebe8ed8fa0d09662f6fbc7e10af4dac1
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-12-09 20:39:05 -0300

    gunicorn update y cambio wrappen css

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,3 @@
-import os
-
-port = os.environ.get("PORT", "10000")  # Render especifica el puerto en una variable de entorno
-command = f"gunicorn core.wsgi:application --bind 0.0.0.0:{port} --env WEB_CONCURRENCY=2"
-os.system(command)
+bind = '0.0.0.0:8000'
+workers = 2
+module = 'core.wsgi:application'
