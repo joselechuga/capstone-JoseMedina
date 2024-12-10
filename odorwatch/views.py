26a5bbbc9c38bdd10a0c0408159e76da3990cf76
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-12-09 23:53:14 -0300

    calidacion usuario, urls a mostrar por correo

--- odorwatch/views.py
+++ odorwatch/views.py
@@ -70,7 +70,6 @@ def loginPage(request):
             return render(request, 'login.html', {'error_message': 'Credenciales incorrectas o falta información'})
     
     return render(request, 'login.html')
-
 # Cerrar sesion
 def logoutUser(request):
     logout(request)
