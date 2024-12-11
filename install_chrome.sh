415f75505e76f505a162fc9627e2183e9ca8f307
Jose Medina DEV <jos.medinav@duocuc.cl>
2024-12-11 15:24:09 -0300

    try fix render

---
+++ install_chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Instalar Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Instalar ChromeDriver
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
+CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/
